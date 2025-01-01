81ec1454c7b70180b3d154031b350e124ab719ec
Abdallahlol <Abdallah.hassan@meti.dev>
2025-01-01 16:32:53 +0200

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,7 @@
+first_number =float(input("enter start year : "))
+second_number =float(input("enter end year : "))
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+print("sum =",sum ,"\n Sub =",subtraction, "\n Multi =",multiplication, "\n Div =",division )
